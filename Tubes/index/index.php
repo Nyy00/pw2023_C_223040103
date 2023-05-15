9916f880f2c9fd17cfc43dce806c3b26316a6d42
Nyy00 <donylks111@gmail.com>
2023-05-15 14:14:55 +0700

    update tubes

---
+++ Tubes/index/index.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>infant health management </title>
+    
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="assets/style.css">
+
+</head>
+<body>
+    
+<!-- header section starts  -->
+
+<header class="header">
+
+    <a href="#" class="logo"> <h3>RS.UTAMA</h3></a>
+    
+
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#about">about</a>
+        <a href="login_doctor/dist/login_doctor.html">Doctor Login</a>
+        <a href="parent_login/dist/parent_login.html">Parent Login</a>
+        <a href="#footer">About us</a>
+
+        
+    </nav>
+    
+    <a href="login/dist/login.html" class="btn btn-danger" role="button">Register</a>
+    
+
+
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="image">
+        <img src="image/doctor.gif" alt="">
+    </div>
+
+    <div class="content">
+        <h1 class="heading"> MOTTO </h1>
+        <h2>"Merawat Penuh Kasih Demi Kesembuhan"</h2>
+
+        <p>Sesuai dengan motto yang diusung, maka RS Utama berusaha untuk selalu memberikan pelayanan dan perawatan yang terbaik sepenuh hati untuk seluruh lapisan masyarakat.</p>
+        <a href="#book" class="btn"><i>Jadwalkan Pertemuan</i><span class="fas fa-chevron-right"></span> </a>
+       
+    </div>
+
+</section>
+<br>
+<br>
+
+
+
+<!-- home section ends -->
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+    <h1 class="heading">about RS.UTAMA </h1>
+
+    <div class="row">
+
+        <div class="image">
+            <img src="image/hospital.gif" alt="">
+        </div>
+
+        <div class="content">
+            <h3>Profile</h3>
+            <p>Berawal dari pengalaman mendirikan Laboratorium Klinik Utama di Tanjungpandan dan Pangkalpinang, serta meningkatnya kepercayaan dan kepuasan masyarakat Bangka Belitung akan layanan yang diberikan, semakin memacu pendiri sekaligus pemilik, dr.Justinus Yusak untuk menyempurnakan dan mengembangkan layanan yang diberikan kepada masyarakat dengan mendirikan Rumah Sakit Utama.</p>
+
+        </div>
+
+    </div>
+
+</section>
+<br>
+<!-- about section ends -->
+
+
+
+<!-- services section starts  -->
+
+<section class="services" id="services">
+
+    <h1 class="heading"> Features </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <i class="fas fa-notes-medical"></i>
+            <h3>POLIKLINIK</h3>
+            <p>Rumah Sakit Utama memiliki beberapa poliklinik, yaitu poliklinik umum, spesialis dan gigi. Poliklinik spesialis terdiri atas poliklinik penyakit dalam, anak, kebidanan dan kandungan, bedah.</p>
+         
+        </div>
+
+        <div class="box">
+            <i class="fas fa-ambulance"></i>
+            <h3>24/7 ambulance</h3>
+            <p>Rumah Sakit Utama memiliki 1 buah ambulance gawat darurat yang dapat mengevakuasi pasien dari dan ke rumah sakit.</p>
+            
+        </div>
+
+        <div class="box">
+            <i class="fas fa-user-md"></i>
+            <h3>INSTALASI GAWAT DARURAT(IGD)</h3>
+            <p>Instalasi Gawat Darurat (IGD) buka 24 jam dengan perawat dan dokter yang siap melayani pasien yang membutuhkan pertolongan gawat darurat. Memiliki tenaga medis dan paramedis yang terlatih di bidang kegawatdaruratan..</p>
+            
+        </div>
+
+        <div class="box">
+            <i class="fas fa-pills"></i>
+            <h3>instalasi farmasi</h3>
+            <p>Instalasi Farmasi Rumah Sakit Utama melayani pasien selama 24 jam, baik pasien internal maupun pasien umum yang membutuhkan obat-obatan 24 jam.</p>
+           
+        </div>
+
+        <div class="box">
+            <i class="fas fa-procedures"></i>
+            <h3>kapasitas</h3>
+            <p>Ruang rawat RS Utama terdiri dari 66 tempat tidur yang terbagi atas :a. Kelas III : 15 tempat Tidur. b. Kelas I : 28 tempat tidur. c. VIP : 2 tempat tidur. d. VVIP : 6 tempat tidur. e. Ruang Isolasi : 1 tempat tidur. f.  Ruang Inkubator : 2 buah. g. Rawatan bayi : 12 tempat tidur</p>
+            
+        </div>
+
+        <div class="box">
+            <i class="fas fa-heartbeat"></i>
+            <h3>INSTALASI GIZI</h3>
+            <p>Adalah unit yang mengelola kegiatan pelayanan gizi rumah sakit secara efektif dan efisien dengan berorientasi pada kepuasan pelanggan baik internal maupun eksternal. Didukung oleh ahli gizi, Instalasi Gizi Rumah Sakit Utama berupaya agar tercipta sistim pelayanan gizi di rumah sakit yang memperhatikan spesifikasi gizi dan penyakit, serta merupakan bagian dari pelayanan kesehatan dalam rangka meningkatkan dan mengembangkan mutu pelayananrumah sakit.</p>
+            
+        </div>
+
+    </div>
+
+</section>
+
+<!-- features section ends -->
+
+
+
+<!-- booking section starts   -->
+
+<section class="book" id="book">
+
+    <h1 class="heading"> <span>JADWALAKAN</span> SEKARANG </h1>    
+
+    <div class="row">
+
+        <div class="image">
+            <img src="https://cdn.dribbble.com/users/2226070/screenshots/5750824/benefit-1-per-dribble.gif" alt="">
+        </div>
+
+        <form action="">
+            <h3>Form Penjadwalan</h3>
+            <input type="text" placeholder="Nama" class="box">
+            <input type="tel" placeholder="Nomor HP" class="box">
+            <input type="email" placeholder="email" class="box">
+            <input type="date" class="box">
+            <input type="submit" value="Jadwalkan!" class="btn">
+        </form>
+
+    </div>
+
+</section>
+
+<!-- booking section ends -->
+
+
+<!-- blogs section starts  -->
+
+<section class="blogs" id="blogs">
+
+    <h1 class="heading"> our <span>Blogs</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+                <img src="https://i.pinimg.com/originals/51/d7/e7/51d7e77c36a68ee327dcb68337222b1e.gif" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                    <a href="#"> <i class="fas fa-calendar"></i> 3 mei, 2022 </a>
+                    
+                </div>
+                <h3>Peduli Anak</h3>
+                <p>Jangan menunda-nunda!.Segera periksakan anak anda ke rumah sakit.</p>
+                <a href="#" class="btn"> Read Full <span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="https://i.pinimg.com/originals/43/00/88/4300881038f42340701e73039c973ee3.gif" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                    <a href="#"> <i class="fas fa-calendar"></i> 11 nov, 2022 </a>
+                    
+                </div>
+                <h3>Pakai Masker!</h3>
+                <p>Untuk menjaga anda dan orang di sekitar anda.</p>
+                <a href="#" class="btn"> Read Full<span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="https://cdn.dribbble.com/users/514480/screenshots/3765609/gif1.gif" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                    <a href="#"> <i class="fas fa-calendar"></i> 20 des, 2022 </a>
+                  
+                </div>
+                <h3>Jaga Kesehatan</h3>
+                <p>Jaga kesehatan dengan olahraga yang cukup setiap harinya.</p>
+                <a href="#" class="btn"> Read Full <span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- blogs section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer" id="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#"> <i class="fas fa-chevron-right"></i> home </a>
+            <a href="#about"> <i class="fas fa-chevron-right"></i> about </a>
+            <a href="#services"> <i class="fas fa-chevron-right"></i> features </a>
+            <a href="#book"> <i class="fas fa-chevron-right"></i> book </a>
+            <a href="#blogs"> <i class="fas fa-chevron-right"></i> blogs </a>
+        </div>
+
+    
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="https://wa.me/6281273736144"> <i class="fas fa-phone"></i> 081273736144</a>
+            
+            <a href="mailto:donylks111@gmail.com"> <i class="fas fa-envelope"></i> donylks111@gmail.com </a>
+            
+            <a href="https://goo.gl/maps/A7oEYhEp4VmxY92D9"> <i class="fas fa-map-marker-alt"></i> Tanjung Pandan,Belitung</a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="https://www.facebook.com/me/"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="https://twitter.com/DonyLaksma"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="https://www.instagram.com/nyy.nyx/?hl=id"> <i class="fab fa-instagram"></i> instagram </a>
+        </div>
+
+    </div>
+
+   
+</section>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
